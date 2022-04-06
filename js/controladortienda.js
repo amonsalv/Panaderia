09e83787db942bdad2fb0f27a8f4b89fd1233b41
206 <206@academicos.cesde.edu.co>
2022-04-06 09:37:37 -0500

    cambios en modal

--- js/controladortienda.js
+++ js/controladortienda.js
@@ -25,7 +25,36 @@ let carrito=[]
 let botonAgregarCarrito=document.getElementById("botonadd")
 botonAgregarCarrito.addEventListener("click",function(){
 
+    //Debo capturar la cantidad y agregarla al producto 
+    let cantidad=document.getElementById("cantidadProducto").value
+    producto.cantidad=cantidad
+
+    //agrego el producto al carrito
+    carrito.push(producto)
+
+    //pintar la capsula en el  carrito
+    let suma=0
+    carrito.forEach(function(producto){
+        suma=suma+Number(producto.cantidad)
+    })
+
+    let capsula=document.getElementById("capsula")
+    capsula.textContent=suma;
+    capsula.classList.remove("invisible")
+
+
     carrito.push(producto)
     console.log(carrito)
     
 })
+
+let btndelete= document.getElementById("btndelete")
+btndelete.addEventListener("click",function(){
+
+    carrito=[]
+    let capsula=document.getElementById("pildora")
+    let capsula.classList.add("invisible")
+})
+
+//rutina para ver el carrito
+
