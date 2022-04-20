a56730c5712772d6428013fa6a583f57af8633db
anamonsalve <anamonsalve96@gmail.com>
2022-04-19 21:34:56 -0500

    carrito

--- js/controladortienda.js
+++ js/controladortienda.js
@@ -7,21 +7,23 @@ let producto={}
 //llamando a tienda llenado
 llenarTienda()
 
+//referencia al modal
+let modalinfo = new bootstrap.Modal(document.getElementById('modalinformacion'))
+let modalcompra = new bootstrap.Modal(document.getElementById('resumencompra'))
+
 // rutina para ampliar informacion
 let contenedorTienda=document.getElementById("filas")
 contenedorTienda.addEventListener("click",function(evento){
 
     if(evento.target.classList.contains("btn")){
-        let modalinfo = new bootstrap.Modal(document.getElementById('modalinfor'))
         producto=ampliarInformacionProducto(evento)
         console.log(producto)
         modalinfo.show()
     }
 })
 
-//rutina par ael anadir a la cesta 
+//rutina para el anadir un producto a la cesta a la cesta 
 let carrito=[]
-
 let botonAgregarCarrito=document.getElementById("botonadd")
 botonAgregarCarrito.addEventListener("click",function(){
 
@@ -42,19 +44,56 @@ botonAgregarCarrito.addEventListener("click",function(){
     capsula.textContent=suma;
     capsula.classList.remove("invisible")
 
-
-    carrito.push(producto)
     console.log(carrito)
-    
+    modalinfo.hide()
 })
 
-let btndelete= document.getElementById("btndelete")
-btndelete.addEventListener("click",function(){
+//rutina para limpiar
+let limpiar=document.getElementById("limpiar")
+limpiar.addEventListener("click",function(){
 
     carrito=[]
-    let capsula=document.getElementById("pildora")
-    let capsula.classList.add("invisible")
+
+    let capsula=document.getElementById("capsula") 
+    capsula.classList.add("invisible")
+
 })
 
-//rutina para ver el carrito
 
+//rutina para ver el carrito
+let botonVerCarrito=document.getElementById("vercarrito")
+botonVerCarrito.addEventListener("click",function(){
+
+    //recorrer el carrito y pintar los productos
+    let base=document.getElementById("basecarro")
+
+    base.innerHTML=""
+
+    carrito.forEach(function(producto){
+
+        let fila=document.createElement("div")
+        fila.classList.add("row")
+
+        let columna1=document.createElement("div")
+        columna1.classList.add("col-4")
+
+        let columna2=document.createElement("div")
+        columna2.classList.add("col-8")
+
+        let foto=document.createElement("img")
+        foto.classList.add("w-100","img-fluid")
+        foto.src=producto.foto
+
+        //PADRES E HIJOS
+        columna1.appendChild(foto)
+        fila.appendChild(columna1)
+        fila.appendChild(columna2)
+        base.appendChild(fila)
+
+    })
+
+    modalcompra.show()
+
+
+
+})
